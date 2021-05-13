ffbeab375590a9e0d075f979429b8cfdde52baf0
caner <canerturkmen@msn.com>
2021-05-14 00:06:29 +0300

    product of array exceptself added

--- leetcode/arrays/productofArrayExceptSelf.py
+++ leetcode/arrays/productofArrayExceptSelf.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #https://leetcode.com/problems/product-of-array-except-self/
 # o(n) time complexity o(n) space comlexity
 
@@ -12,3 +13,17 @@ def productExceptSelf(self, nums: List[int]) -> List[int]:
         right_prod *= nums[i]             
         
     return result
+=======
+#https://leetcode.com/problems/product-of-array-except-self
+#o(n) time o(n) space 
+def productExceptSelf(nums: List[int]) -> List[int]:
+    output = [1] * len(nums)
+        for i in range(1, len(nums)):
+            output[i] = nums[i-1] * output[i-1]
+            
+        right_prod = 1 
+        for i in range(len(nums)-1, -1, -1):
+            output[i] = output[i] * right_prod
+            right_prod = right_prod * nums[i]
+        return output
+>>>>>>> Stashed changes
